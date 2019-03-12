838d8b08f96aefed48b98603bab18f4356b1b79a
Christopher Luna <chris@illusionlayervfx.com>
2019-03-12 05:16:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif array.length == 3
     value = "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
-    value = array[0..-2].join(", ") + " and " + array[-1]
+    value = array[0..-1].join(", ") + " and " + array[-1]
   end
 end
